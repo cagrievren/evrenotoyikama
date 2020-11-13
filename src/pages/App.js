21fcf1dfaaeed8be7b5463c853e3313901a4d692
Çağrı Evren <cagrievren93@gmail.com>
2020-11-13 21:55:47 +0300

    app is created

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    
+  };
+
+  render() {
+    return (
+      <div>Evren Oto Yıkama</div>
+    );
+  }
+}
+
+export default App;
